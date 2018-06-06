090f4751a2e255792ed7c01df74b06d52aad22d3
ratster <nikhil.khangar@wingify.com>
2018-06-06 17:13:18 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/c97c49cb23f8ca88abe2c6e802d9bc67.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/c97c49cb23f8ca88abe2c6e802d9bc67.js');
